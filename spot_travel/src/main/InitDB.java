92d49de1f38aa61ce1fcbee6c484ac06655c1c7e
Jackie-Han-Jiachang <jackie.han1002@icloud.com>
2025-05-21 12:42:46 +0800

    Update Spring Boot into the project, and a H2 tcp database is setup

--- spot_travel/src/main/InitDB.java
+++ spot_travel/src/main/InitDB.java
@@ -4,8 +4,8 @@ import java.sql.DriverManager;
 public class InitDB {
     public static void main(String[] args) throws Exception {
         Connection conn = DriverManager.getConnection(
-            "jdbc:h2:file:/Users/jackyhan/Java/h2/data/traveldb", "sa", "");
+            "jdbc:h2:file:/Users/jackyhan/Java/h2/data/attractionDB", "sa", "");
         conn.close();
-        System.out.println("✅ 连接成功，数据库文件应已生成");
+        System.out.println("连接成功，数据库文件应已生成");
     }
 }
