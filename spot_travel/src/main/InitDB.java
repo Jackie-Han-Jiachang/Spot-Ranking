d2d120c27e0817fc0393f2b3104567411a9fc100
Jackie-Han-Jiachang <jackie.han1002@icloud.com>
2025-05-22 19:42:22 +0800

    Write source of the H2 database connection

--- spot_travel/src/main/InitDB.java
+++ spot_travel/src/main/InitDB.java
@@ -2,6 +2,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class InitDB {
+
+    /*
+    Source: https://www.h2database.com/html/tutorial.html#tutorial_starting_h2_console
+    Connecting to a Database using JDBC, bacuse the database is not created yet, we can use the file path to create a new database.
+        */
     public static void main(String[] args) throws Exception {
         Connection conn = DriverManager.getConnection(
             "jdbc:h2:file:/Users/jackyhan/Java/h2/data/attractionDB", "sa", "");
