8814d5b741ea934c22baeaa8d13a9e5c9feaaf6f
Jackie-Han-Jiachang <jackie.han1002@icloud.com>
2025-05-24 12:48:03 +0800

    Frondend for adding a new review

--- spot_travel/src/main/resources/schema.sql
+++ spot_travel/src/main/resources/schema.sql
@@ -1,6 +1,16 @@
 -- Initiate empty table
+-- source: https://forcedotcom.github.io/phoenix/
 CREATE TABLE attractions(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE reviews(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    attraction_id INT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    comment TEXT NOT NULL,
+    rating INT NOT NULL,
+    FOREIGN KEY (attraction_id) REFERENCES attractions(id)
 );
--- spot_travel/target/classes/static/app.js
+++ spot_travel/target/classes/static/app.js
@@ -1,111 +1,113 @@
-$(document).ready(function() {
-    // function loadAttraction() {
-    //     $.get("/api/attractions", function(data) {
-    //         $("#attractionList").empty();
-    //         data.forEach(attraction => {
-    //             // add the attraction to the list
-    //             $("#attractionList").append(`
-    //                 <li>
-    //                 ${attraction.name} - ${attraction.location}
-    //                 <button onclick="removeAttraction('${attraction.id}')"> Remove </button>
-    //                 </li>
-    //             `);
-    //         });
-    //     });
-    // }
-    // // add an location (name and location)
-    // function addAttraction() {
-    //     const attraction ={
-    //         name: $("#attractionName").val(),
-    //         location: $("#attractionLocation").val()
-    //     };
-    // // use ajax to post the data to the server
-    // $.ajax({
-    //     // api post and get the datas from the server
-    //     url: "/api/attractions",
-    //     type: "POST",
-    //     contentType: "application/json",
-    //     data: JSON.stringify(attraction),
-    //     success: function() {
-    //         $("#addAttractionForm")[0].reset();
-    //         loadAttraction();
-    //     },error: function() {
-    //         alert("Add failed, please try again...");
-    //     }
-    // });
-    // }
+$(document).ready(function () {
+  // main function
 
-    // delete an attraction
-    // function removeAttraction(id) {
-    //     $.ajax({
-    //         url: "/api/attractions/" + id,
-    //         type: "DELETE",
-    //         success: function() {
-    //             loadAttraction();
-    //         },
-    //         error: function() {
-    //             alert("Delete failed, please try again...");
-    //         }
-    //     });
-    // }
-
-    // operator
-    if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
-        loadAttraction();
-        $("#addAttractionForm").on("submit", function(event) {
-            event.preventDefault();
-            addAttraction();
-        });
-    }
+  // home page
+  if (
+    window.location.pathname === "/index.html" ||
+    window.location.pathname === "/"
+  ) {
+    loadAttraction();
+    $("#addAttractionForm").on("submit", function (event) {
+      event.preventDefault();
+      addAttraction();
+    });
+  }
+  // add review page
+   if (
+    window.location.pathname === "/addReview.html" ||
+    window.location.pathname === "/addReview"
+   ) {
+    const urlParams = new URLSearchParams(window.location.search);
+    attractionId = urlParams.get("id");
+    loadAttractionForReview(attractionId);
+    $("#addReviewForm").on("submit", function (event) {
+      event.preventDefault();
+      addReview(attractionId);
+    });
+  }
 });
 
 function loadAttraction() {
-        $.get("/api/attractions", function(data) {
-            $("#attractionList").empty();
-            data.forEach(attraction => {
-                // add the attraction to the list
-                $("#attractionList").append(`
+  $.get("/api/attractions", function (data) {
+    $("#attractionList").empty();
+    data.forEach((attraction) => {
+      // add the attraction to the list
+      $("#attractionList").append(`
                     <li>
                     ${attraction.name} - ${attraction.location}
                     <button onclick="removeAttraction('${attraction.id}')"> Remove </button>
+                    <a href="addReview.html?id=${attraction.id}"> <button> New Review </button </a>
                     </li>
                 `);
-            });
-        });
-    }
+    });
+  });
+}
+
+function loadAttractionForReview(attractionId) {
+  $.get("/api/attractions/" + attractionId, function (data) {
+    $("#attractionName").text(data.name);
+    $("#attractionLocation").text(data.location);
+    }).fail(function() {
+        alert("Failed to load attraction data");
+        window.location.href = "/index.html";
+    });
+  };
 
 // add an location (name and location)
-    function addAttraction() {
-        const attraction ={
-            name: $("#attractionName").val(),
-            location: $("#attractionLocation").val()
-        };
-    // use ajax to post the data to the server
-    $.ajax({
-        // api post and get the datas from the server
-        url: "/api/attractions",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(attraction),
-        success: function() {
-            $("#addAttractionForm")[0].reset();
-            loadAttraction();
-        },error: function() {
-            alert("Add failed, please try again...");
-        }
-    });
-    }
+function addAttraction() {
+  const attraction = {
+    name: $("#attractionName").val(),
+    location: $("#attractionLocation").val(),
+  };
+  // use ajax to post the data to the server
+  $.ajax({
+    // api post and get the datas from the server
+    url: "/api/attractions",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(attraction),
+    success: function () {
+      $("#addAttractionForm")[0].reset();
+      loadAttraction();
+    },
+    error: function () {
+      alert("Add failed, please try again...");
+    },
+  });
+}
 
 // delete an attraction
-    function removeAttraction(id) {
-        $.ajax({
-            url: "/api/attractions/" + id,
-            type: "DELETE",
-            success: function() {
-                loadAttraction();
-            },
-            error: function() {
-                alert("Delete failed, please try again...");
-            }
-        });
-    }
+function removeAttraction(id) {
+  $.ajax({
+    url: "/api/attractions/" + id,
+    type: "DELETE",
+    success: function () {
+      loadAttraction();
+    },
+    error: function () {
+      alert("Delete failed, please try again...");
+    },
+  });
+}
+
+function addReview(id) {
+  const review = {
+    attraction: { id: attractionId },
+    username: $("#userName").val(),
+    comment: $("#comment").val(),
+    grade: parseInt($("#grade").val()),
+  };
+  $.ajax({
+    url: "api/reviews",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(review),
+    success: function () {
+      alert("Review added successfully");
+      window.location.href = "/index.html";
+    },
+    error: function () {
+      alert("Failed to add review");
+    },
+  });
+}
