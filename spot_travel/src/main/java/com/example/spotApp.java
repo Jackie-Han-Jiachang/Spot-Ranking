9617a10cffa1eccc56ec8c220240c3dfc0c9c9ef
Jackie-Han-Jiachang <jackie.han1002@icloud.com>
2025-05-30 11:30:10 +0800

    Create an executable jar.

--- spot_travel/src/main/java/com/example/spotApp.java
+++ spot_travel/src/main/java/com/example/spotApp.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // source: https://spring.io/guides/gs/spring-boot
 
 @SpringBootApplication
 public class SpotApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
+    Connection conn = DriverManager.getConnection("jdbc:h2:file:./data/attractionDB", "sa", "");
     SpringApplication.run(SpotApp.class, args);
+    conn.close();
   }
 }
